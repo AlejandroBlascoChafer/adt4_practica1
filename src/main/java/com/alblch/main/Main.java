bfbb784ec95c5118ec96129bc764f6f53ccd4578
Alejandro <alexblascochafer16@gmail.com>
2024-10-30 12:58:52 +0100

    vc

---
+++ src/main/java/com/alblch/main/Main.java
@@ -0,0 +1,11 @@
+package com.alblch.main;
+
+import com.alblch.vista.JFEmpleados;
+
+public class Main {
+    public static void main(String[] args) {
+        JFEmpleados vista = new JFEmpleados();
+
+
+    }
+}
---
+++ src/main/java/com/alblch/vista/JFEmpleados.java
@@ -0,0 +1,24 @@
+package com.alblch.vista;
+
+import javax.swing.*;
+
+public class JFEmpleados {
+    private JPanel pnPrincipal;
+    private JPanel pnDerecha;
+    private JPanel pnIzquierda;
+    private JPanel pnSuperior;
+    private JPanel pnInferior;
+    private JTextField txtDni;
+    private JTextField txtNombre;
+    private JTextField txtApellidos;
+    private JTextField txtEdad;
+    private JTextField txtSalario;
+    private JButton btnInsertar;
+    private JButton btnListar;
+    private JButton btnBorrar;
+    private JTable jtEmpleados;
+
+    public JFEmpleados() {
+
+    }
+}
